6b0dfa1777a8a6adf4b09298d0d7d620d4673e85
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-30 19:30:53 +0000

    Deploy pavitra71994/my-app to github.com/pavitra71994/my-app.git:gh-pages

---
+++ precache-manifest.06babd4d301e50b65d46b5e4700b10db.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc4c06537dd5972a50d5f61251f39c8",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "72cb7c85059a74fad6d2",
+    "url": "/my-app/static/css/main.1a717bd5.chunk.css"
+  },
+  {
+    "revision": "6c959b287ae0646cd715",
+    "url": "/my-app/static/js/2.cfb7070c.chunk.js"
+  },
+  {
+    "revision": "d37fa7fee1e0ab0ac7ba4b4e561d770c",
+    "url": "/my-app/static/js/2.cfb7070c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72cb7c85059a74fad6d2",
+    "url": "/my-app/static/js/main.6e20f803.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "1612bffb4132941d227af1804a6fdb4d",
+    "url": "/my-app/static/media/16.1612bffb.jpg"
+  },
+  {
+    "revision": "972884aa17a97aaaf786d879d2435e0f",
+    "url": "/my-app/static/media/answered.972884aa.svg"
+  },
+  {
+    "revision": "1f4838d92a19e450da9845fff4ae600a",
+    "url": "/my-app/static/media/books-690219.1f4838d9.jpg"
+  },
+  {
+    "revision": "1e7e841189b27736743ef96873fb8d89",
+    "url": "/my-app/static/media/check.1e7e8411.svg"
+  },
+  {
+    "revision": "32dca033b7e878868f38c50052e6dcb8",
+    "url": "/my-app/static/media/error.32dca033.svg"
+  },
+  {
+    "revision": "6b2102070cd190fe331b0581f7e435df",
+    "url": "/my-app/static/media/help.6b210207.svg"
+  },
+  {
+    "revision": "e654163987cf9ff7b80c5fe4f051a79d",
+    "url": "/my-app/static/media/list.e6541639.svg"
+  },
+  {
+    "revision": "797d5a5cacbe68fd2741e3a30f446719",
+    "url": "/my-app/static/media/rec.797d5a5c.svg"
+  },
+  {
+    "revision": "bd3e556a5d95d0ea9de9680b89295cac",
+    "url": "/my-app/static/media/stopwatch.bd3e556a.svg"
+  }
+]);
